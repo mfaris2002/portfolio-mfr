f4b2b1081c81fd4658ae09c78e429f64834ad4c8
mfaris2002 <farisrafi2002@gmail.com>
2026-05-02 21:10:16 +0700

    fix: revert to simplified TS configs and ensure proper compilation in Dockerfile

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,6 +1,6 @@
-const path = require('path');
+import path from 'path';
 
-module.exports = ({ env }) => ({
+export default ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
